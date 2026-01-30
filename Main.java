7596063ac0cc63e09001ce4e1183a899474951c3
Kanykeyy <zaksylykovakanykej23@gmail.com>
2026-01-30 16:17:36 +0600

    my first commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+
+        //1. books
+
+        ArrayList<String> books1  = new ArrayList<>();
+        books1.add("Bible");
+        books1.add("Harry Potter");
+        books1.add("The Little Prince");
+        books1.add("Pride and Prejudice");
+        books1.add("The Alchemist");
+
+
+        ArrayList<String> books2 = new ArrayList<>();
+        books2.add("The Great Gatsby");
+        books2.add("Harry Potter");
+        books2.add("To Kill a Mockingbird");
+        books2.add("1984");
+        books2.add("The Alchemist");
+
+
+        ArrayList<String> result = new ArrayList<>();
+        for(String c : books2) {
+
+        }
+
+
+
+
+
+
+
+
+
+        }
+    }
